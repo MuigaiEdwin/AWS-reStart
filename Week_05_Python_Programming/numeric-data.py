a69c4bb7d04b3e420771161c5e100a10f48b48cc
Muigai Edwin <muigaiedd@gmail.com>
2025-10-12 09:00:43 +0300

    3 Numeric Data types

---
+++ Week_05_Python_Programming/numeric-data.py
@@ -0,0 +1,39 @@
+# Lab: Working with Numeric Data Types
+# ------------------------------------
+# In this lab, we explore Python's numeric data types:
+# int, float, complex, and bool.
+
+print("Python has three numeric types: int, float, and complex")
+
+# Exercise 2: int data type
+myValue = 1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Exercise 3: float data type
+myValue = 3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Exercise 4: complex data type
+myValue = 5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Exercise 5: bool data type (True)
+myValue = True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Exercise 5 continued: bool data type (False)
+myValue = False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# End of Lab
+print("\nCongratulations! You have successfully explored Python numeric data types.")

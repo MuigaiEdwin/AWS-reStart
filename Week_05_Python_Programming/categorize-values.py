9f0e1a69e77de820c9e0df7a2d89175e0c117615
Muigai Edwin <muigaiedd@gmail.com>
2025-10-14 23:56:37 +0300

    added the collection & category files

---
+++ Week_05_Python_Programming/categorize-values.py
@@ -0,0 +1,7 @@
+
+#Exercise 1: Creating a mixed-type list
+
+myMixedTypeList = [45, 290578, 1.02, True, "My first November Year of Consistency"]
+
+for item in myMixedTypeList:
+    print("{} is of the data type {}".format(item,type(item)))
